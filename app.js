18fe0c11d27564c257c8fcde791d60bc6a59ef03
Agus Pribadi <aguspribadi368@gmail.com>
2023-06-28 14:06:24 +0700

    memberikana animasi loading

--- app.js
+++ app.js
@@ -1,62 +1,56 @@
 function convertImage() {
-    // Mendapatkan input elemen gambar
     var inputImage = document.getElementById("image");
 
-    // Memeriksa apakah gambar telah dipilih
     if (inputImage.files.length === 0) {
         alert("Silakan pilih gambar!");
         return;
     }
 
-    // Mendapatkan file gambar yang dipilih
     var imageFile = inputImage.files[0];
 
-    // Membuat objek FileReader
     var reader = new FileReader();
-
-    // Event listener saat pembacaan file selesai
     reader.onload = function (event) {
-        // Membuat elemen gambar
         var img = document.createElement("img");
         img.src = event.target.result;
 
-        // Mendapatkan format output yang dipilih
         var formatSelect = document.getElementById("format");
         var format = formatSelect.value;
 
-        // Mengubah format gambar menggunakan canvas
         var canvas = document.createElement("canvas");
         var ctx = canvas.getContext("2d");
         canvas.width = img.width;
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0);
 
-        // Mengubah format gambar menggunakan canvas
-        var canvas = document.createElement("canvas");
-        var ctx = canvas.getContext("2d");
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-
-        // Mengambil data gambar yang sudah diubah formatnya
         var convertedImage = canvas.toDataURL("image/" + format);
 
-        // Membuat elemen tautan unduhan
         var downloadLink = document.createElement("a");
         downloadLink.href = convertedImage;
         downloadLink.download = "converted_image." + format;
+
         downloadLink.innerHTML = "Unduh";
 
-        // Mendapatkan elemen hasil
         var resultDiv = document.getElementById("result");
-
-        // Membersihkan hasil sebelumnya (jika ada)
         resultDiv.innerHTML = "";
 
-        // Menambahkan elemen tautan unduhan ke elemen hasil
+        var previewContainer = document.createElement("div");
+        previewContainer.classList.add("preview-container");
+
+        var previewImage = document.createElement("img");
+        previewImage.classList.add("preview-image");
+        previewImage.src = convertedImage;
+
+        previewContainer.appendChild(previewImage);
+        resultDiv.appendChild(previewContainer);
         resultDiv.appendChild(downloadLink);
+
+        resultDiv.classList.remove("loading");
     };
 
-    // Membaca file gambar sebagai data URL
+    reader.onloadstart = function () {
+        var resultDiv = document.getElementById("result");
+        resultDiv.classList.add("loading");
+    };
+
     reader.readAsDataURL(imageFile);
 }
